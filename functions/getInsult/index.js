5f7d3e9fee455cfe872bee82cd04d3f01433947b
unknown <you@example.com>
2024-09-11 23:21:55 +0200

    first commit

---
+++ functions/getInsult/index.js
@@ -0,0 +1,37 @@
+const  { sendResponse, sendError } = require("../../responses/index");
+const {db} = require("../../services/db");
+
+
+
+const insults = [
+    {
+        insult: "Never hung poison on a fouler toad",
+        play: "Rickard III"
+    },
+    {
+        insult: "He thinks too much: such men are dangerous. ",
+        play: "Julius Ceasar"
+    }
+];
+
+
+
+exports.handler = async (event) => {
+    try {
+        const { Items } = await db.scan({ 
+            TableName: "insults-db",
+        FilterExpression: "attribute_exists(#DYNOBASE_insult)",
+        ExpressionAttributeNames: { 
+            "#DYNOBASE_insult": "insult" 
+        }
+        
+        
+        }).promise();
+        return sendResponse(200, {success: true, insults :Items});
+    } catch (error) {
+        return sendError(500, {
+            success: false,
+            message: 'could not get insults'
+        });
+    }
+};
